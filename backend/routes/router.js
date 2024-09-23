bd6ebdf42489e09135b9d5e34ddcff562a35c50c
Teemu <teemuikonen@live.fi>
2024-09-23 15:15:57 +0300

    modified api

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,7 @@
+const { json } = require("body-parser");
 const express = require("express");
 const fs = require("fs");
+const { type } = require("os");
 const path = require("path");
 const router = express.Router();
 
@@ -13,6 +15,46 @@ router.get("/images", (req, res) => {
     }
     const images = JSON.parse(data);
     res.json(images);
+  });
+});
+
+router.get("/ratings", (req, res) => {
+  const filePath = path.join(__dirname, "../rating.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading rating.json:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    const ratings = JSON.parse(data);
+    res.json(ratings);
+  });
+});
+
+router.post("/ratings", (req, res) => {
+  const filePath = path.join(__dirname, "../rating.json");
+  const newRate = req.body; // Access the request body
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading rating.json:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    let oldRate = JSON.parse(data);
+
+    for (let i = 0; i < oldRate.length; i++) {
+      oldRate[i].push(newRate[i]);
+    }
+
+    fs.writeFile(filePath, JSON.stringify(oldRate), "utf8", (err) => {
+      if (err) {
+        console.error("Error writing to rating.json:", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      res.status(201).send("Rating added successfully");
+    });
   });
 });
 
--- frontend/src/imgBox.js
+++ frontend/src/imgBox.js
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from "react";
-//import jsonData from "./images.json";
 import Zoom from "react-medium-image-zoom";
 import axios from "axios";
 
 function ImgBox() {
-  /*
-  let jsonData = { images: [] };
-  axios
-    .get("http://localhost:3001/images")
-    .then((response) => (jsonData = console.log(response.data)));*/
   const [jsonData, setJsonData] = useState({ images: [] });
 
   useEffect(() => {
