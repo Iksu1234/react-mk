199dd59066b38f01a260317ffa5afc7b730e9a7c
Teemu <teemuikonen@live.fi>
2024-09-24 15:02:33 +0300

    voting functionality added

--- frontend/src/imgBox.js
+++ frontend/src/imgBox.js
@@ -14,9 +14,8 @@ function ImgBox() {
         console.error("Error fetching data:", error);
       }
     };
-
     fetchData();
-  }, []);
+  });
 
   return (
     <div className="pageWrapper">
@@ -36,10 +35,45 @@ function ImgBox() {
               ))}
             </div>
           </div>
+          <input
+            id={`r${index}`}
+            type="number"
+            min={1}
+            max={10}
+            step={0.1}
+            placeholder="rating 1-10"
+          ></input>
         </div>
       ))}
+      <div>
+        <button id="sendBtn" onClick={sendRatings}>
+          Send ratings
+        </button>
+      </div>
     </div>
   );
+  function sendRatings() {
+    const ratings = [];
+    try {
+      for (let i = 0; i < jsonData.images.length; i++) {
+        const rating = Number(document.getElementById(`r${i}`).value);
+        if (rating < 1 || rating > 10 || isNaN(rating)) {
+          alert("Ratings must be between 1 and 10");
+          return;
+        } else {
+          ratings.push(rating);
+        }
+      }
+      axios.post("/ratings", ratings, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(ratings);
+    } catch (error) {
+      console.error("Error sending data:", error);
+    }
+  }
 }
 
 export default ImgBox;
